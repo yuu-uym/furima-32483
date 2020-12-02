a69f82ce92b4415b1f0e03af9bb08adf2b37e791
yuu-uym <uyama0816y@gmail.com>
2020-12-02 19:05:41 +0900

    migrate

--- db/migrate/20201202092619_devise_create_users.rb
+++ db/migrate/20201202092619_devise_create_users.rb
@@ -4,6 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :kana_first,         null: false
+      t.string :kana_family,        null: false
+      t.date   :birth,              null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
