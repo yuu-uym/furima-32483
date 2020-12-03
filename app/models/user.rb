aaefd3d13166d380738ef4977f50025e36f71267
yuu-uym <uyama0816y@gmail.com>
2020-12-03 14:32:53 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name,   presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :kana_first,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :kana_family, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth,       presence: true
-  validates :nickname,    presence: true
+
+  with_options presence: true do
+    validates :birth
+    validates :nickname
+        
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+      validates :first_name
+      validates :last_name
+    end
+        
+     with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+       validates :kana_first
+       validates :kana_family
+     end
+   end
+                
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
