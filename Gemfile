26c4ecf4b69be6f0e271a90b0cb2102f0acd3da2
yuu-uym <uyama0816y@gmail.com>
2020-12-03 17:21:00 +0900

    ActiveHashの設定とバリデーションの設定

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'pry-rails'
+gem 'active_hash'
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,19 @@ class Item < ApplicationRecord
 
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :prefectures
+  belongs_to_active_hash :day_to_delivery
+
+  validates :title, :text, presence: true
+  validates :value, :text, presence: true
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
+  validates :prefectures_id, numericality: { other_than: 1 } 
+  validates :day_to_delivery_id, numericality: { other_than: 1 } 
 end
