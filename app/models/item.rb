b46e71d9bbcd70adee1417587fc7beb672a4bef0
yuu-uym <uyama0816y@gmail.com>
2020-12-04 15:39:37 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,7 @@ class Item < ApplicationRecord
     validates :name
     validates :value
     validates :description
+    validates :image
 
     with_options numericality: { other_than: 1 }  do
       validates :category_id
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                        {Faker::Lorem.sentence}
+    description                 {Faker::Lorem.sentence}
+    category_id                 {Faker::Number.between(from: 2, to: 11)}
+    condition_id                {Faker::Number.between(from: 2, to: 6)}
+    delivery_fee_id             {Faker::Number.between(from: 2, to: 4)}
+    prefecture_id               {Faker::Number.between(from: 2, to: 48)}
+    day_to_delivery_id          {Faker::Number.between(from: 2, to: 3)}
+    value                       {Faker::Number.between(from: 300, to: 9999999)}
+    association :user 
   end
 end
