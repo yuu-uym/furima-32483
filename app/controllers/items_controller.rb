32dd519b7d777c10b02685da95fb81bb791dcd9b
yuu-uym <uyama0816y@gmail.com>
2020-12-05 16:21:24 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,11 @@ class ItemsController < ApplicationController
     end
    end
 
+   def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+   end
 
   private
 
