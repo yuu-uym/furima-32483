e585d222780cb5e623fa81d040145728ea493cd2
yuu-uym <uyama0816y@gmail.com>
2020-12-05 15:19:05 +0900

    ルーティングなど

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,11 +31,11 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    # if @item.update(item_params)
-    #    redirect_to  item_path
-    # else 
-    #   render :edit
-    # end
+    if @item.update(item_params)
+       redirect_to item_path
+    else 
+      render :edit
+    end
    end
 
 
