5d245fcd0cbd4185fbd673b5dabf1f4f67f4c8ee
yuu-uym <uyama0816y@gmail.com>
2020-12-05 15:51:02 +0900

    before_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @item = Item.all.order("created_at DESC")
@@ -19,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
        redirect_to item_path
     else 
@@ -43,5 +41,9 @@ class ItemsController < ApplicationController
 
     def item_params
       params.require(:item).permit(:name, :description,:category_id, :condition_id,  :delivery_fee_id, :prefecture_id, :day_to_delivery_id, :value, :image).merge(user_id: current_user.id)
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
     end
   end
