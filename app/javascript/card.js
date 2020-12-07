d690b20911feb324cf6b70e435606731d6fe8fec
yuu-uym <uyama0816y@gmail.com>
2020-12-07 17:57:10 +0900

    クレジット　フォームに追加

---
+++ app/javascript/card.js
@@ -0,0 +1,35 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_d2bf3d04ef9b99facd1a2ca9"); 
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("order_number").removeAttribute("name");
+      document.getElementById("order_cvc").removeAttribute("name");
+      document.getElementById("order_exp_month").removeAttribute("name");
+      document.getElementById("order_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener("load", pay);
