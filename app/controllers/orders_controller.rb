68771c01bc29840de4a6cb6e6b45f04085628efc
yuu-uym <uyama0816y@gmail.com>
2020-12-08 16:39:04 +0900

    before_action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:create, :index]
 
   def index
-    @item = Item.find(params[:item_id])
     if @item.user_id == current_user.id || @item.order.present?
       redirect_to root_path
     else
@@ -16,7 +16,6 @@ class OrdersController < ApplicationController
 
   def create
     @user_order = UserOrder.new(order_params)
-    @item = Item.find(params[:item_id])
     if @user_order.valid?
       Payjp.api_key = ENV['PAYJP_SECRET_KEY']
       Payjp::Charge.create(
@@ -36,4 +35,9 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:user_order).permit(:prefecture_id, :city, :address, :building_name, :phone_number, :postal_code).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
