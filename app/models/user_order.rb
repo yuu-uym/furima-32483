8efd8d6afd53bffce107c0afc4235eb0017ac372
yuu-uym <uyama0816y@gmail.com>
2020-12-08 17:10:52 +0900

    不要なコードの削除

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -15,7 +15,6 @@ class UserOrder
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
