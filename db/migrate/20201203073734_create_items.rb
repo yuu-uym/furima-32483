8fdaaed204eb222247232e67db5022fa51b46785
yuu-uym <uyama0816y@gmail.com>
2020-12-29 11:18:28 +0900

    調整

--- db/migrate/20201203073734_create_items.rb
+++ db/migrate/20201203073734_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name,         null: false
-      t.text   :description,         null: false
+      t.string :name,                  null: false
+      t.text   :description,           null: false
       t.integer  :category_id,         null: false
-      t.integer  :condition_id,         null: false
-      t.integer :delivery_fee_id,         null: false
-      t.integer :prefecture_id,         null: false
-      t.integer :day_to_delivery_id,         null: false
-      t.integer :value,         null: false
-      t.references :user,         null: false, foreign_key: true 
+      t.integer  :condition_id,        null: false
+      t.integer :delivery_fee_id,      null: false
+      t.integer :prefecture_id,        null: false
+      t.integer :day_to_delivery_id,   null: false
+      t.integer :value,                null: false
+      t.references :user,              null: false, foreign_key: true 
       t.timestamps
     end
   end
